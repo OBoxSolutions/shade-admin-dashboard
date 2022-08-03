5b782c2903583619010aa963af96b0c2670444ad
Elodin <jcassola96@gmail.com>
2022-08-03 14:40:14 -0400

    set up user mutations

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,15 +1,34 @@
 import adminApi from "@/api/admin";
 
 //Login user
-export const login = async ({ commit }, loginData) => {
+export const login = async ({ dispatch }, loginData) => {
   const { data } = await adminApi.post("/login", loginData)
 
   const { status } = data
 
   if (status === 1){
-    commit("loginUser", data.token);
+    dispatch("attempt", data)
   }
   return data
+}
+
+export const attempt = async ({ commit }, userData) => {
+  const { access_token } = userData
+  commit("setToken", access_token)
+
+  try {
+    const { data } = await adminApi.get("/user-profile", {
+      headers: {
+        'Authorization': 'Bearer ' + access_token
+      }
+    })
+    commit("setUser", data)
+
+    return data
+
+  } catch (error) {
+    console.log('failure')
+  }
 }
 
 //Logout current user
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,5 @@
 export default () => ({
-  token: "",
+  token: '',
+  user: null,
   messages: [],
 });
