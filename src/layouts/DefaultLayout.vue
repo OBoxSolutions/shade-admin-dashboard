60f1eae2fe4a7f57686b14a9b8cb20435fad37d3
Jose Garcia <jogarcia@tuta.io>
2022-09-25 10:59:24 -0400

    Add logo

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,9 +1,15 @@
 <template>
   <v-app>
     <v-app-bar color="primary" dark app>
+      <img src="@/assets/logo.svg" class="logo" />
+
       <v-row justify="end">
-        <v-app-bar-nav-icon v-if="authenticated" title="Logout" @click="logoutUser"> 
-          <v-icon>mdi-logout</v-icon> 
+        <v-app-bar-nav-icon
+          v-if="authenticated"
+          title="Logout"
+          @click="logoutUser"
+        >
+          <v-icon>mdi-logout</v-icon>
         </v-app-bar-nav-icon>
       </v-row>
     </v-app-bar>
@@ -15,21 +21,28 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex"
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "DefaultLayout",
-   computed: {
-        ...mapGetters('auth', ['authenticated', 'user']),
-   },
-   methods: {
-    ...mapActions('auth', ['logout']),
-    async logoutUser(){
-      const resp = await this.logout()
-      if(resp){
-        this.$router.replace({ name: "Login" })
+  computed: {
+    ...mapGetters("auth", ["authenticated", "user"]),
+  },
+  methods: {
+    ...mapActions("auth", ["logout"]),
+    async logoutUser() {
+      const resp = await this.logout();
+      if (resp) {
+        this.$router.replace({ name: "Login" });
       }
-    }
-   },
-}
+    },
+  },
+};
 </script>
+
+<style scoped>
+.logo {
+  height: 100%;
+  width: auto;
+}
+</style>
