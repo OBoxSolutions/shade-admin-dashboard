737f3a048cb3618afc8bc0fd334129969fedc9bf
Jose Garcia <jogarcia@tuta.io>
2022-09-25 16:35:09 -0400

    Add social buttons

--- src/components/ContactsRequestForm.vue
+++ src/components/ContactsRequestForm.vue
@@ -11,6 +11,26 @@
       append-icon="mdi-magnify"
       label="Search"
     ></v-text-field>
+    <div class="d-flex flex-column">
+      <v-label> Social </v-label>
+      <v-btn-toggle v-model="filterData.value" multiple>
+        <v-btn value="facebook">
+          <v-icon>mdi-facebook-messenger</v-icon>
+        </v-btn>
+
+        <v-btn value="instagram">
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+
+        <v-btn value="discord">
+          <v-icon>mdi-discord</v-icon>
+        </v-btn>
+
+        <v-btn value="email">
+          <v-icon>mdi-email</v-icon>
+        </v-btn>
+      </v-btn-toggle>
+    </div>
   </v-form>
 </template>
 
