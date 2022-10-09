f693b2dd43b1bd6a634b35451b6e15de298dfaa4
Jose Garcia <jogarcia@tuta.io>
2022-10-09 15:29:28 -0400

    Add views

---
+++ src/views/VideoMeetingView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <messages-table class="table"></messages-table>
+  </div>
+</template>
+
+<script>
+import MessagesTable from "@/components/MessagesTable.vue";
+
+export default {
+  name: "VideoMeetingView",
+  components: {
+    MessagesTable,
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+.table {
+  width: 90%;
+}
+</style>
