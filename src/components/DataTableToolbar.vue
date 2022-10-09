2f195249ef5b50b2d6676d8ed97ab43de24f6b07
Jose Garcia <jogarcia@tuta.io>
2022-10-09 16:02:56 -0400

    Create DataTableToolbar component

---
+++ src/components/DataTableToolbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="d-flex justify-end">
+    <v-text-field
+      v-model="searchComputed"
+      prepend-inner-icon="mdi-magnify"
+      class="search-bar mr-2"
+    ></v-text-field>
+    <v-btn
+      @click="onReload"
+      icon
+      class="ma-2"
+      outlined
+      fab
+      small
+      color="primary"
+    >
+      <v-icon>mdi-cached</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataTableToolbar",
+  props: {
+    search: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    searchComputed: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit("update:search", val);
+      },
+    },
+  },
+  methods: {
+    onReload() {
+      this.$emit("click:reload");
+    },
+  },
+};
+</script>
+
+<style></style>
