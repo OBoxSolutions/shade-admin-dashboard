8f29b9547161bd2596c729178c9b9153f70a937e
Jose Garcia <jogarcia@tuta.io>
2022-10-09 16:35:18 -0400

    Set basic VideoMeetingView

--- src/views/VideoMeetingView.vue
+++ src/views/VideoMeetingView.vue
@@ -1,16 +1,77 @@
 <template>
   <div class="home">
-    <messages-table class="table"></messages-table>
+    <data-table-toolbar
+      :search.sync="search"
+      @click:reload="loadData"
+    ></data-table-toolbar>
+
+    <v-data-table
+      :headers="headers"
+      :items="meetings"
+      class="elevation-2"
+      :loading="loading"
+      loading-text="Loading video meetings..."
+      no-data-text="No video meetings found"
+      :search="search"
+    >
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="showSelectedMessage(item)"
+          color="primary"
+        >
+          mdi-eye
+        </v-icon>
+        <v-icon small @click="deleteSelectedMessage(item)" color="error">
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
-import MessagesTable from "@/components/MessagesTable.vue";
+import { getVideoMeetings } from "@/api/video-meeting";
+
+import DataTableToolbar from "@/components/DataTableToolbar.vue";
 
 export default {
   name: "VideoMeetingView",
   components: {
-    MessagesTable,
+    DataTableToolbar,
+  },
+  data() {
+    return {
+      meetings: [],
+      search: "",
+      loading: false,
+      headers: [
+        { text: "Name", value: "name" },
+        { text: "Email", value: "email" },
+        { text: "Country", value: "country" },
+        { text: "Birthdate", value: "birthdate" },
+        { text: "App", value: "app" },
+        { text: "Meeting Date", value: "meeting-date" },
+        { text: "Time Before Meeting", value: "time-before-meeting" },
+        { text: "Meeting Link", value: "meeting-link" },
+        { text: "Actions", value: "actions" },
+      ],
+    };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      this.loading = true;
+      try {
+        this.meetings = (await (await getVideoMeetings()).data?.data) ?? [];
+      } catch (error) {
+        console.log(error);
+      }
+      this.loading = false;
+    },
   },
 };
 </script>
