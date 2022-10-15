fd379c025561dde1652013ce7314e8b21a03f5a7
Jose Garcia <jogarcia@tuta.io>
2022-10-15 19:59:57 -0400

    Set theme

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style scoped>
+:root {
+  --primary-color: "#11ABAB";
+  --secondary-color: "#FEAC07";
+  --action-color: "#FF4E01";
+  --accent-color: "#0065B8";
+  --contrast-color: "#ffffff";
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#11ABAB",
+        secondary: "#FEAC07",
+        action: "#FF4E01",
+        accent: "#0065B8",
+        contrast: "#ffffff",
+      },
+    },
+  },
+});
