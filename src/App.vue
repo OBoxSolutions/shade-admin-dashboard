bd4ac34a81609d384e1704f92648a73e9e8dab6b
Jose Garcia <jogarcia@tuta.io>
2022-10-15 20:59:56 -0400

    Add general fonts

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
 </template>
 
 <style>
+body {
+  font-family: Prompt, "Courier New", Courier, monospace;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Press Start 2P", "Franklin Gothic Medium", "Arial Narrow", Arial,
+    sans-serif;
+}
+
 :root {
   --primary-color: #0065b8;
   --secondary-color: #feac07;
