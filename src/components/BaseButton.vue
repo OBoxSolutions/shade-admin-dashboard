80097342e9251a66450d879cea7f7e28de33666d
Jose Garcia <jogarcia@tuta.io>
2022-10-15 20:58:08 -0400

    Make BaseButton transparent component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn class="base-button" v-bind="$attrs" v-on="$listeners" color="action">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+};
+</script>
+
+<style scoped>
+.base-button {
+  font-family: "Press Start 2P", "Franklin Gothic Medium", "Arial Narrow", Arial,
+    sans-serif;
+}
+</style>
