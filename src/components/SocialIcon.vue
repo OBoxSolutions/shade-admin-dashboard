cc6d8e424ba18a345eedb4685b7eeb9e470f583c
Jose Garcia <jogarcia@tuta.io>
2022-10-23 13:19:29 -0400

    Add twitter

--- src/components/SocialIcon.vue
+++ src/components/SocialIcon.vue
@@ -1,14 +1,17 @@
 <template>
-    <div>
-        <v-icon large left v-if="social === 'Messanger'">mdi-facebook-messenger</v-icon>
-        <v-icon large left v-if="social === 'Instagram'">mdi-instagram</v-icon>
-        <v-icon large left v-if="social === 'Discord'">mdi-discord</v-icon>
-        <v-icon large left v-if="social === 'Email'">mdi-email</v-icon>
-    </div>
+  <div>
+    <v-icon large left v-if="social === 'Twitter'">mdi-twitter</v-icon>
+    <v-icon large left v-if="social === 'Messanger'"
+      >mdi-facebook-messenger</v-icon
+    >
+    <v-icon large left v-if="social === 'Instagram'">mdi-instagram</v-icon>
+    <v-icon large left v-if="social === 'Discord'">mdi-discord</v-icon>
+    <v-icon large left v-if="social === 'Email'">mdi-email</v-icon>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['social']
-}
+  props: ["social"],
+};
 </script>
