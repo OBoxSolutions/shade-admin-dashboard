0262a118171135a5f0bd477ea6c256aa27372729
Jose Garcia <jogarcia@tuta.io>
2022-11-06 16:35:32 -0500

    Create mapper of questions

---
+++ src/utils/mapper.js
@@ -0,0 +1,11 @@
+export const questionsMap = {
+  question_one: "What is your proudest accomplishment?",
+  question_two: "What moral value do you value the most?",
+  question_three: "Are you ever justified to kill?",
+  question_four: "Is morality subjective or objective?",
+  question_five: "If you could live forever, would do you want to?",
+  question_six: "If you could change the world, what would you change?",
+  question_seven: "Some questions?",
+  question_eight: "Are you satisfied with yourself?",
+  question_nine: "Maybe have anything to add? Any questions perhaps?",
+};
