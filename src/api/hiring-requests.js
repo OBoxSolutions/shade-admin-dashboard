61950358c4c325bd464c3000432bfab3dcf24b2c
Jose Garcia <jogarcia@tuta.io>
2022-11-06 16:02:18 -0500

    Create delete hiring request service

--- src/api/hiring-requests.js
+++ src/api/hiring-requests.js
@@ -4,3 +4,8 @@ export async function getHiringRequests() {
   const chatMeetings = await adminApi.get("hiring-requests");
   return chatMeetings;
 }
+
+export async function deleteHiringRequests(id) {
+  const chatMeetings = await adminApi.delete(`hiring-requests/${id}`);
+  return chatMeetings;
+}
