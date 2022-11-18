bc383ccfebbbfa62f623838bb08102c9e4a9e12f
Jose Garcia <jogarcia@tuta.io>
2022-11-18 11:46:14 -0500

    Add deleteChatMeeting service

--- src/api/chat-meeting.js
+++ src/api/chat-meeting.js
@@ -4,3 +4,8 @@ export async function getChatMeetings() {
   const chatMeetings = await adminApi.get("chat-meetings");
   return chatMeetings;
 }
+
+export async function deleteChatMeeting(id) {
+  const response = await (await adminApi.delete(`chat-meetings/${id}`)).data;
+  return response;
+}
